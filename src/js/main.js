a531c47da2906eeb9d72f6940d6de8589af45b3c
Dmitriy Kudlovich <carnagery@mail.ru>
2021-12-20 06:22:47 +0300

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+// Swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var container = document.querySelector(".container");
+document.querySelectorAll(".container")
